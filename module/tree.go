18298d1c8bb105efdfc1ec8e86c879a29ccb82c9
wanglisheng <wanglisheng_i@didiglobal.com>
2021-08-19 13:58:13 +0800

    二叉树和链表的基本结构

--- module/tree.go
+++ module/tree.go
@@ -5,3 +5,20 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func NewTreeNode(val int) *TreeNode {
+	return &TreeNode{
+		Val: val,
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{
+		Val: val,
+	}
+}
