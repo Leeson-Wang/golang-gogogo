c01e0d838d9701aae4eb721c84dc1eee9f8338ec
wanglisheng <wanglisheng_i@didiglobal.com>
2021-08-19 14:50:56 +0800

    写一个http服务

--- logic/http_client_server.go
+++ logic/http_client_server.go
@@ -1,7 +1,28 @@
 package logic
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func Httptest() {
 	//客户端
+	client := &http.Client{}
+
+	url := "http://www.baidu.com"
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Do(request)
+
+	stdout := os.Stdout
+	io.Copy(stdout, response.Body)
+
+	code := response.StatusCode
+	fmt.Println(code)
 }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "golang-gogogo/logic"
+
+func main() {
+	logic.Httptest()
+}
